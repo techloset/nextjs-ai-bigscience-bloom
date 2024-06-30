718f0296dd22f44e0fe9705df024b2a26cf352ff
naveed-rana <rana.naveed812@gmail.com>
2024-06-30 12:09:12 +0500

    docker file added

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contact() {
+    return (
+       <div>
+       <h1>test</h1>
+       </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HuggingFaceQuery from '../components/HuggingFaceQuery'
 export default function Home() {
   return (
     <>
+    <h1>App</h1>
     <HuggingFaceQuery/>
     </>
   )
